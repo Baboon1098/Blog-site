ecc8faa54825ebffea47bdd2602233c9d6c3dba8
Baboon1098 <137217755+Baboon1098@users.noreply.github.com>
2023-12-13 21:42:05 -0600

    a few changes

--- seeds/createSeeds.js
+++ seeds/createSeeds.js
@@ -2,6 +2,10 @@ const { writeFileSync } = require("fs")
 const { faker } = require('@faker-js/faker');
 const path = require("path")
 
+// FOR RUNNING INSIDE THE TECHBLOG CHALLENGE
+// npm i @faker-js/faker
+// Run createSeeds first, then run seed. Both files must be in seeds dir in root of project
+
 const RANDOMUSERS = [];
 
 function createRandomUser() {
@@ -18,6 +22,25 @@ Array.from({ length: 10 }).forEach(() => {
 });
 
 console.log(RANDOMUSERS)
+
+/////////////////////  USE NEW postdata.json /////////////////////
+
+// const POSTS = [];
+
+// function createRandomPost() {
+//     return {
+//         title: faker.lorem.words(Math.ceil(Math.random() * 5)),
+//         content: faker.lorem.paragraphs(Math.ceil(Math.random() * 4)),
+//         date_created: faker.date.between('2020-01-01T00:00:00.000Z', '2023-01-01T00:00:00.000Z'),
+//         user_id: Math.ceil(Math.random() * RANDOMUSERS.length)
+//     };
+// }
+
+// Array.from({ length: 15 }).forEach(() => {
+//     POSTS.push(createRandomPost());
+// });
+
+// console.log(POSTS)
 
 const COMMENTS = [];
 
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,23 +1,22 @@
 const sequelize = require('../config/connection');
-const { User, Post } = require('../models');
+const { User, Post, Comment } = require('../models');
 
-const userData = require('./userData.json');
-const postData = require('./postData.json');
+const userData = require('./user_data.json');
+const postData = require('./postdata.json');
+const commentData = require('./comment_data.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const users = await User.bulkCreate(userData, {
+  await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
 
-  for (const post of postData) {
-    await Post.create({
-      ...post,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
+  await Post.bulkCreate(postData)
+  await Comment.bulkCreate(commentData)
+
+  console.log("Seeding done!");
 
   process.exit(0);
 };
